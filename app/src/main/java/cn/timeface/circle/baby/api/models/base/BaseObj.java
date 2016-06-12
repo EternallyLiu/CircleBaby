768478c4bddf2d91400bdd7d7ca0bc626aeacd8f
zhangsheng <zhangsheng@timeface.cn>
2016-06-12 09:47:10 +0800

    Parcelable

--- app/src/main/java/cn/timeface/circle/baby/api/models/base/BaseObj.java
+++ app/src/main/java/cn/timeface/circle/baby/api/models/base/BaseObj.java
@@ -4,7 +4,6 @@ package cn.timeface.circle.baby.api.models.base;
  * author: rayboot  Created on 15/12/3.
  * email : sy0725work@gmail.com
  */
-public class BaseObj implements Serializable {
-    private static final long serialVersionUID = 1L;
 public class BaseObj {
 }
+
--- app/src/main/java/cn/timeface/circle/baby/api/models/objs/MediaObj.java
+++ app/src/main/java/cn/timeface/circle/baby/api/models/objs/MediaObj.java
@@ -9,8 +9,7 @@ import cn.timeface.circle.baby.api.models.base.BaseObj;
  * Created by lidonglin on 2016/5/9.
  */
 public class MediaObj extends BaseObj
-//        implements Parcelable
-{
+        implements Parcelable {
     String content;         //图片描述
     int h;                  //长度
     int w;                  //宽度
@@ -22,7 +21,7 @@ public class MediaObj extends BaseObj
     String videoUrl;        //视频url
 
     //图片
-    public MediaObj(String content,String imgUrl, int w , int h ,long photographTime) {
+    public MediaObj(String content, String imgUrl, int w, int h, long photographTime) {
         this.content = content;
         this.imgUrl = imgUrl;
         this.w = w;
@@ -123,45 +122,45 @@ public class MediaObj extends BaseObj
         this.videoUrl = videoUrl;
     }
 
-//    @Override
-//    public int describeContents() {
-//        return 0;
-//    }
-//
-//    @Override
-//    public void writeToParcel(Parcel dest, int flags) {
-//        dest.writeString(this.content);
-//        dest.writeInt(this.h);
-//        dest.writeInt(this.w);
-//        dest.writeInt(this.id);
-//        dest.writeString(this.imgUrl);
-//        dest.writeLong(this.length);
-//        dest.writeString(this.localPath);
-//        dest.writeLong(this.photographTime);
-//        dest.writeString(this.videoUrl);
-//    }
-//
-//    protected MediaObj(Parcel in) {
-//        this.content = in.readString();
-//        this.h = in.readInt();
-//        this.w = in.readInt();
-//        this.id = in.readInt();
-//        this.imgUrl = in.readString();
-//        this.length = in.readLong();
-//        this.localPath = in.readString();
-//        this.photographTime = in.readLong();
-//        this.videoUrl = in.readString();
-//    }
-//
-//    public static final Parcelable.Creator<MediaObj> CREATOR = new Parcelable.Creator<MediaObj>() {
-//        @Override
-//        public MediaObj createFromParcel(Parcel source) {
-//            return new MediaObj(source);
-//        }
-//
-//        @Override
-//        public MediaObj[] newArray(int size) {
-//            return new MediaObj[size];
-//        }
-//    };
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.content);
+        dest.writeInt(this.h);
+        dest.writeInt(this.w);
+        dest.writeInt(this.id);
+        dest.writeString(this.imgUrl);
+        dest.writeLong(this.length);
+        dest.writeString(this.localPath);
+        dest.writeLong(this.photographTime);
+        dest.writeString(this.videoUrl);
+    }
+
+    protected MediaObj(Parcel in) {
+        this.content = in.readString();
+        this.h = in.readInt();
+        this.w = in.readInt();
+        this.id = in.readInt();
+        this.imgUrl = in.readString();
+        this.length = in.readLong();
+        this.localPath = in.readString();
+        this.photographTime = in.readLong();
+        this.videoUrl = in.readString();
+    }
+
+    public static final Parcelable.Creator<MediaObj> CREATOR = new Parcelable.Creator<MediaObj>() {
+        @Override
+        public MediaObj createFromParcel(Parcel source) {
+            return new MediaObj(source);
+        }
+
+        @Override
+        public MediaObj[] newArray(int size) {
+            return new MediaObj[size];
+        }
+    };
 }
