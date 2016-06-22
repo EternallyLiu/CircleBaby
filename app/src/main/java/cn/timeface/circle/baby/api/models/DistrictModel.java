56cda6ef07a3bd95d85f97cb8d528eebd99dee0e
lidonglin <lidonglin@timeface.cn>
2016-06-22 18:34:17 +0800

    订单提交及接口

---
+++ app/src/main/java/cn/timeface/circle/baby/api/models/DistrictModel.java
@@ -0,0 +1,64 @@
+package cn.timeface.circle.baby.api.models;
+
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+import com.activeandroid.query.Delete;
+import com.activeandroid.query.Select;
+import com.bluelinelabs.logansquare.annotation.JsonObject;
+
+import java.util.List;
+
+/**
+ * @author SunYanwei (QQ:707831837)
+ * @from 2014年9月11日下午2:02:38
+ * @TODO 省市区字典
+ */
+@Table(name = "district_table")
+@JsonObject(fieldDetectionPolicy = JsonObject.FieldDetectionPolicy.NONPRIVATE_FIELDS_AND_ACCESSORS)
+public class DistrictModel extends Model {
+    @Column(name = "locationId")
+    public String locationId;
+
+    @Column(name = "locationName")
+    public String locationName;
+
+    @Column(name = "locationPid")
+    public String locationPid;
+
+    /**
+     * 查询省,市,区
+     */
+    public static DistrictModel query(long proId) {
+
+        return new Select().from(DistrictModel.class).where("locationId = ?", proId)
+                .executeSingle();
+    }
+
+    public static DistrictModel query(String id) {
+
+        return new Select().from(DistrictModel.class).where("locationId = ?", id)
+                .executeSingle();
+    }
+
+    public static DistrictModel queryByName(String name) {
+
+        return new Select().from(DistrictModel.class).where("locationName = ?", name)
+                .executeSingle();
+    }
+
+    public static List<DistrictModel> queryDicts() {
+        return new Select().from(DistrictModel.class).where("locationPid = ?", "0")
+                .execute();
+    }
+
+    public static List<DistrictModel> queryDictsByParentId(String id) {
+        return new Select().from(DistrictModel.class).where("locationPid = ?", id)
+                .execute();
+    }
+
+    public static void deleteAll() {
+        new Delete().from(DistrictModel.class).execute();
+    }
+
+}
