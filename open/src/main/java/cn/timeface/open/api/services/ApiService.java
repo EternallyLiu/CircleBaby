95caf42eae26e4cf1c45bf408595fc4814388b99
石岩 <shiyan@timeface.cn>
2016-07-08 17:42:09 +0800

    更多接口

--- open/src/main/java/cn/timeface/open/api/services/ApiService.java
+++ open/src/main/java/cn/timeface/open/api/services/ApiService.java
@@ -29,9 +29,9 @@ public interface ApiService {
     /**
      * 请求开放平台授权token,token是使用开放平台功能的唯一凭证，token存在有效期，过期后需要重新请求接口获取。 token期限需要第三方自行维护。
      *
-     * @param app_id
-     * @param app_secret
-     * @param user_object
+     * @param app_id 第三方在时光流影开放平台上的app_id
+     * @param app_secret 第三方在时光流影开放平台上的app_secret
+     * @param user_object 用户基本信息模型
      * @return
      */
     @POST("api/authorize")
@@ -41,11 +41,14 @@ public interface ApiService {
 
     /**
      * 开放平台一键成书功能,第三方传入指定类型数据，通过pod接口生成可用在线阅读或者二次编辑的排版数据
-     *
-     * @param book_id
-     * @param book_type
-     * @param rebuild
-     * @param content_list
+     * @param book_id 时光书唯一ID
+     * @param book_type 时光书类型ID
+     * @param rebuild 是否重新排版生成，1 重新生成 0 使用原有排版数据
+     * @param content_list 存在http body中的拍版内容数据
+     * @param book_info 是否请求书信息（不包含正文内容页，仅仅包含封面，封底等信息）
+     * @param page_size 分页查询每页记录数,最大60
+     * @param current_page 分页查询当前页码，pod支持分页获取数据，不传默认获取全部数据
+     * @param read 	1 返回可供在线翻阅的阅读地址， 0 返回二次排版数据
      * @return
      */
     @FormUrlEncoded
@@ -53,15 +56,28 @@ public interface ApiService {
     Observable<BaseResponse<TFOBookModel>> getPOD(@Field("book_id") String book_id,
                                                   @Field("book_type") int book_type,
                                                   @Field("rebuild") String rebuild,
+                                                  @Field("content_list") String content_list,
+                                                  @Field("book_info") int book_info,
+                                                  @Field("page_size") int page_size,
+                                                  @Field("current_page") int current_page,
+                                                  @Field("read") int read);
+
+    @FormUrlEncoded
+    @POST("pod/pod")
+    Observable<BaseResponse<TFOBookModel>> getPOD(@Field("book_type") int book_type,
                                                   @Field("content_list") String content_list);
+
+    @FormUrlEncoded
+    @POST("pod/pod?book_info=1")
+    Observable<BaseResponse<TFOBookModel>> bookInfo(@Field("book_id") int book_id);
 
     /**
      * 修改文字接口
      *
-     * @param book_id
-     * @param content_id
-     * @param element_model
-     * @param text
+     * @param book_id 时光书ID
+     * @param content_id 需要修改文字所在页编码
+     * @param element_model 当前文字所在element_model
+     * @param text 	需要修改的文字
      * @return
      */
     @FormUrlEncoded
@@ -74,8 +90,8 @@ public interface ApiService {
     /**
      * 通过修改POD接口返回Content单页内容。修改是一个全量修改的过程，为了保持书内容的完整性，每一次修改都需要提交完成的修改数据。
      *
-     * @param book_id
-     * @param content_list
+     * @param book_id 时光书ID
+     * @param content_list 需要修改的书页数据集合
      * @return
      */
     @FormUrlEncoded
@@ -86,7 +102,7 @@ public interface ApiService {
     /**
      * 开放平台提供自定义封面功能，请求接口获取时光书封面模板列表。
      *
-     * @param bookType
+     * @param bookType 版式类型ID，针对版式有私有封面的情况
      * @return
      */
     @GET("api/templatelist")
@@ -95,8 +111,8 @@ public interface ApiService {
     /**
      * 开放平台提供自定义封面功能，请求接口获取时光书封面模板详情
      *
-     * @param template_id
-     * @param book_id
+     * @param template_id 	封面模板ID
+     * @param book_id 时光书ID
      * @return
      */
     @GET("api/templateinfo")
@@ -106,11 +122,11 @@ public interface ApiService {
     /**
      * 时光书创建成功后，获取完整封面数据用于编辑封面
      *
-     * @param template_id
-     * @param book_id
-     * @param book_title
-     * @param book_auth
-     * @param content_list
+     * @param template_id 时光书封面模板ID
+     * @param book_id 时光书ID
+     * @param book_title 时光书标题
+     * @param book_auth 	时光书作者姓名，如果不存在会调用注册时候传入的用户信息
+     * @param content_list 封面模板和封底修改后的数据
      * @return
      */
     @FormUrlEncoded
@@ -133,8 +149,8 @@ public interface ApiService {
     /**
      * 获取用户已创建的时光书列表
      *
-     * @param page_size
-     * @param current_page
+     * @param page_size 分页查询每页记录数,最大60
+     * @param current_page 分页查询当前页码
      * @return
      */
     @GET("api/booklist")
@@ -142,22 +158,30 @@ public interface ApiService {
                                                 @Query("current_page") int current_page);
 
 
-    /**
-     * 获取用户已创建的时光书列表
-     *
-     * @return
-     */
     @GET("api/booklist")
     Observable<BaseResponse<BookList>> bookList();
 
-    @POST("api/attachlist")
+
+    /**
+     * 获取编辑内容挂件
+     * @param book_id 时光书ID
+     * @param request_type 1 背景 2 挂件 3 色值组
+     * @param book_type 版式类型
+     * @return
+     */
+    @GET("api/attachlist")
     Observable<BaseResponse<List<TFBookBgModel>>> getAttachBgList(@Query("book_id") String book_id,
                                                                   @Query("request_type") int request_type,
                                                                   @Query("book_type") String book_type);
 
     /**
      * 创建一本时光书
-     *
+     * @param book_type 时光书版式类型
+     * @param book_title 时光书标题
+     * @param template_id 时光书封面模板ID
+     * @param book_summary 时光书描述,会印刷在勒口上
+     * @param book_summary_image_url 时光书勒口头像，如果不存在会调用注册时候传入的用户信息
+     * @param book_auth 时光书作者姓名，如果不存在会调用注册时候传入的用户信息
      * @return
      */
     @FormUrlEncoded
@@ -176,7 +200,7 @@ public interface ApiService {
 
     /**
      * 删除一本时光书
-     *
+     * @param book_id 时光书ID
      * @return
      */
     @FormUrlEncoded
@@ -185,27 +209,13 @@ public interface ApiService {
 
     /**
      * Page 重新排版
-     *
+     * @param content_ids 需要重新排版的2页id
      * @return
      */
     @FormUrlEncoded
     @POST("api/reformat")
     Observable<BaseResponse> reformat(@Field("content_ids") String content_ids);
 
-    /**
-     * 获取编辑内容挂件
-     *
-     * @param book_id
-     * @param request_type
-     * @param book_type
-     * @return
-     */
-    @POST("api/attachlist")
-    Observable<BaseResponse<List<TFOBookImageModel>>> getAttachPendantList(@Query("book_id") String book_id,
-                                                                           @Query("request_type") int request_type,
-                                                                           @Query("book_type") String book_type);
 
-    @POST("api/bookcover")
-    Observable<BaseResponse> getBookcover(@Query("book_id") String book_id);
 
 }
