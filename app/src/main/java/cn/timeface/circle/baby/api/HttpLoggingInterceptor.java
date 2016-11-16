4c5653ce263da21bd36d99c741b0551afecad697
shiyan <shiyan@timeface.cn>
2016-11-16 11:38:42 +0800

    升级log

--- app/src/main/java/cn/timeface/circle/baby/api/HttpLoggingInterceptor.java
+++ app/src/main/java/cn/timeface/circle/baby/api/HttpLoggingInterceptor.java
@@ -1,58 +1,67 @@
 package cn.timeface.circle.baby.api;
 
+
+import java.io.EOFException;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.nio.charset.UnsupportedCharsetException;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.Connection;
 import okhttp3.Headers;
 import okhttp3.Interceptor;
 import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
 import okhttp3.Protocol;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
-import okhttp3.internal.Platform;
-import okhttp3.internal.http.HttpEngine;
+import okhttp3.internal.http.HttpHeaders;
+import okhttp3.internal.platform.Platform;
 import okio.Buffer;
 import okio.BufferedSource;
 
-import static okhttp3.internal.Platform.INFO;
+import static okhttp3.internal.platform.Platform.INFO;
 
 /**
- * author: rayboot  Created on 16/1/12.
- * email : sy0725work@gmail.com
+ * An OkHttp interceptor which logs request and response information. Can be applied as an
+ * {@linkplain OkHttpClient#interceptors() application interceptor} or as a {@linkplain
+ * OkHttpClient#networkInterceptors() network interceptor}. <p> The format of the logs created by
+ * this class should not be considered stable and may change slightly between releases. If you need
+ * a stable logging format, use your own interceptor.
  */
-public class HttpLoggingInterceptor implements Interceptor {
+public final class HttpLoggingInterceptor implements Interceptor {
     private static final Charset UTF8 = Charset.forName("UTF-8");
 
     public enum Level {
-        /** No logs. */
+        /**
+         * No logs.
+         */
         NONE,
         /**
          * Logs request and response lines.
-         *
+         * <p>
          * <p>Example:
          * <pre>{@code
-         * --> POST /greeting HTTP/1.1 (3-byte body)
+         * --> POST /greeting http/1.1 (3-byte body)
          *
-         * <-- HTTP/1.1 200 OK (22ms, 6-byte body)
+         * <-- 200 OK (22ms, 6-byte body)
          * }</pre>
          */
         BASIC,
         /**
          * Logs request and response lines and their respective headers.
-         *
+         * <p>
          * <p>Example:
          * <pre>{@code
-         * --> POST /greeting HTTP/1.1
+         * --> POST /greeting http/1.1
          * Host: example.com
          * Content-Type: plain/text
          * Content-Length: 3
          * --> END POST
          *
-         * <-- HTTP/1.1 200 OK (22ms)
+         * <-- 200 OK (22ms)
          * Content-Type: plain/text
          * Content-Length: 6
          * <-- END HTTP
@@ -61,18 +70,18 @@ public class HttpLoggingInterceptor implements Interceptor {
         HEADERS,
         /**
          * Logs request and response lines and their respective headers and bodies (if present).
-         *
+         * <p>
          * <p>Example:
          * <pre>{@code
-         * --> POST /greeting HTTP/1.1
+         * --> POST /greeting http/1.1
          * Host: example.com
          * Content-Type: plain/text
          * Content-Length: 3
          *
          * Hi?
-         * --> END GET
+         * --> END POST
          *
-         * <-- HTTP/1.1 200 OK (22ms)
+         * <-- 200 OK (22ms)
          * Content-Type: plain/text
          * Content-Length: 6
          *
@@ -86,8 +95,10 @@ public class HttpLoggingInterceptor implements Interceptor {
     public interface Logger {
         void log(String message);
 
-        /** A {@link Logger} defaults output appropriate for the current platform. */
-        Logger DEFAULT = new Logger() {
+        /**
+         * A {@link HttpLoggingInterceptor.Logger} defaults output appropriate for the current platform.
+         */
+        HttpLoggingInterceptor.Logger DEFAULT = new HttpLoggingInterceptor.Logger() {
             @Override
             public void log(String message) {
                 Platform.get().log(INFO, message, null);
@@ -96,46 +107,48 @@ public class HttpLoggingInterceptor implements Interceptor {
     }
 
     public HttpLoggingInterceptor() {
-        this(Logger.DEFAULT);
+        this(HttpLoggingInterceptor.Logger.DEFAULT);
     }
 
-    public HttpLoggingInterceptor(Logger logger) {
+    public HttpLoggingInterceptor(HttpLoggingInterceptor.Logger logger) {
         this.logger = logger;
     }
 
-    private final Logger logger;
+    private final HttpLoggingInterceptor.Logger logger;
 
-    private volatile Level level = Level.NONE;
+    private volatile HttpLoggingInterceptor.Level level = HttpLoggingInterceptor.Level.NONE;
 
-    /** Change the level at which this interceptor logs. */
-    public HttpLoggingInterceptor setLevel(Level level) {
+    /**
+     * Change the level at which this interceptor logs.
+     */
+    public HttpLoggingInterceptor setLevel(HttpLoggingInterceptor.Level level) {
         if (level == null) throw new NullPointerException("level == null. Use Level.NONE instead.");
         this.level = level;
         return this;
     }
 
-    public Level getLevel() {
+    public HttpLoggingInterceptor.Level getLevel() {
         return level;
     }
 
-    @Override public Response intercept(Chain chain) throws IOException {
-        Level level = this.level;
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+        HttpLoggingInterceptor.Level level = this.level;
 
         Request request = chain.request();
-        if (level == Level.NONE) {
+        if (level == HttpLoggingInterceptor.Level.NONE) {
             return chain.proceed(request);
         }
 
-        boolean logBody = level == Level.BODY;
-        boolean logHeaders = logBody || level == Level.HEADERS;
+        boolean logBody = level == HttpLoggingInterceptor.Level.BODY;
+        boolean logHeaders = logBody || level == HttpLoggingInterceptor.Level.HEADERS;
 
         RequestBody requestBody = request.body();
         boolean hasRequestBody = requestBody != null;
 
         Connection connection = chain.connection();
         Protocol protocol = connection != null ? connection.protocol() : Protocol.HTTP_1_1;
-        String requestStartMessage =
-                "--> " + request.method() + ' ' + request.url() + ' ' + protocol(protocol);
+        String requestStartMessage = "--> " + request.method() + ' ' + request.url() + ' ' + protocol;
         if (!logHeaders && hasRequestBody) {
             requestStartMessage += " (" + requestBody.contentLength() + "-byte body)";
         }
@@ -173,25 +186,37 @@ public class HttpLoggingInterceptor implements Interceptor {
                 Charset charset = UTF8;
                 MediaType contentType = requestBody.contentType();
                 if (contentType != null) {
-                    contentType.charset(UTF8);
+                    charset = contentType.charset(UTF8);
                 }
 
                 logger.log("");
-                logger.log(buffer.readString(charset));
-
-                logger.log("--> END " + request.method()
-                        + " (" + requestBody.contentLength() + "-byte body)");
+                if (isPlaintext(buffer)) {
+                    logger.log(buffer.readString(charset));
+                    logger.log("--> END " + request.method()
+                            + " (" + requestBody.contentLength() + "-byte body)");
+                } else {
+                    logger.log("--> END " + request.method() + " (binary "
+                            + requestBody.contentLength() + "-byte body omitted)");
+                }
             }
         }
 
         long startNs = System.nanoTime();
-        Response response = chain.proceed(request);
+        Response response;
+        try {
+            response = chain.proceed(request);
+        } catch (Exception e) {
+            logger.log("<-- HTTP FAILED: " + e);
+            throw e;
+        }
         long tookMs = TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - startNs);
 
         ResponseBody responseBody = response.body();
+        long contentLength = responseBody.contentLength();
+        String bodySize = contentLength != -1 ? contentLength + "-byte" : "unknown-length";
         logger.log("<-- " + response.code() + ' ' + response.message() + ' '
                 + response.request().url() + " (" + tookMs + "ms" + (!logHeaders ? ", "
-                + responseBody.contentLength() + "-byte body" : "") + ')');
+                + bodySize + " body" : "") + ')');
 
         if (logHeaders) {
             Headers headers = response.headers();
@@ -199,7 +224,7 @@ public class HttpLoggingInterceptor implements Interceptor {
                 logger.log(headers.name(i) + ": " + headers.value(i));
             }
 
-            if (!logBody || !HttpEngine.hasBody(response)) {
+            if (!logBody || !HttpHeaders.hasBody(response)) {
                 logger.log("<-- END HTTP");
             } else if (bodyEncoded(response.headers())) {
                 logger.log("<-- END HTTP (encoded body omitted)");
@@ -211,10 +236,24 @@ public class HttpLoggingInterceptor implements Interceptor {
                 Charset charset = UTF8;
                 MediaType contentType = responseBody.contentType();
                 if (contentType != null) {
-                    charset = contentType.charset(UTF8);
+                    try {
+                        charset = contentType.charset(UTF8);
+                    } catch (UnsupportedCharsetException e) {
+                        logger.log("");
+                        logger.log("Couldn't decode the response body; charset is likely malformed.");
+                        logger.log("<-- END HTTP");
+
+                        return response;
+                    }
                 }
 
-                if (responseBody.contentLength() != 0) {
+                if (!isPlaintext(buffer)) {
+                    logger.log("");
+                    logger.log("<-- END HTTP (binary " + buffer.size() + "-byte body omitted)");
+                    return response;
+                }
+
+                if (contentLength != 0) {
                     logger.log("");
                     logger.log(buffer.clone().readString(charset));
                 }
@@ -226,12 +265,32 @@ public class HttpLoggingInterceptor implements Interceptor {
         return response;
     }
 
+    /**
+     * Returns true if the body in question probably contains human readable text. Uses a small sample
+     * of code points to detect unicode control characters commonly used in binary file signatures.
+     */
+    static boolean isPlaintext(Buffer buffer) {
+        try {
+            Buffer prefix = new Buffer();
+            long byteCount = buffer.size() < 64 ? buffer.size() : 64;
+            buffer.copyTo(prefix, 0, byteCount);
+            for (int i = 0; i < 16; i++) {
+                if (prefix.exhausted()) {
+                    break;
+                }
+                int codePoint = prefix.readUtf8CodePoint();
+                if (Character.isISOControl(codePoint) && !Character.isWhitespace(codePoint)) {
+                    return false;
+                }
+            }
+            return true;
+        } catch (EOFException e) {
+            return false; // Truncated UTF-8 sequence.
+        }
+    }
+
     private boolean bodyEncoded(Headers headers) {
         String contentEncoding = headers.get("Content-Encoding");
         return contentEncoding != null && !contentEncoding.equalsIgnoreCase("identity");
-    }
-
-    private static String protocol(Protocol protocol) {
-        return protocol == Protocol.HTTP_1_0 ? "HTTP/1.0" : "HTTP/1.1";
     }
 }
