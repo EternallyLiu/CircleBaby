744187d75efd1c25579d8d70c991fcc442c34206
汪帅 <wangbin@timeface.cn>
2017-01-12 10:09:12 +0800

    1、完成时光轴时间选择界面 2、调整展开和收起箭头的大小位置 3、实现一个SelectImageView以便于控制展开和收起 4、更换点赞图标

---
+++ app/src/main/java/cn/timeface/circle/baby/ui/timelines/views/SelectImageView.java
@@ -0,0 +1,11 @@
+package cn.timeface.circle.baby.ui.timelines.views;
+
+import android.widget.ImageView;
+import android.widget.Checkable;
+
+/**
+ * Created by wangshuai on 2017/1/12.
+ */
+
+public class SelectImageView extends ImageView implements Checkable {
+}
