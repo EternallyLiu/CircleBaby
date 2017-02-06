d026e62ae623d2a1b1c0b87e9b29f6d2114f04b7
汪帅 <wangbin@timeface.cn>
2017-02-06 17:16:12 +0800

    完善加载视频资源  排除缩略图重复生成的问题，简化视频页面代码！

--- app/src/main/java/cn/timeface/circle/baby/support/api/models/VideoInfo.java
+++ app/src/main/java/cn/timeface/circle/baby/support/api/models/VideoInfo.java
@@ -5,6 +5,7 @@ import android.media.ThumbnailUtils;
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.provider.MediaStore;
+import android.text.TextUtils;
 
 import com.raizlabs.android.dbflow.annotation.Column;
 import com.raizlabs.android.dbflow.annotation.ColumnIgnore;
@@ -14,6 +15,7 @@ import com.raizlabs.android.dbflow.config.FlowManager;
 import com.raizlabs.android.dbflow.sql.language.SQLite;
 import com.raizlabs.android.dbflow.structure.BaseModel;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -144,6 +146,46 @@ public class VideoInfo extends BaseModel implements Parcelable {
     public static void saveAll(List<VideoInfo> list) {
         if (list != null && list.size() > 0)
             FlowManager.getModelAdapter(VideoInfo.class).saveAll(list);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        VideoInfo videoInfo = (VideoInfo) o;
+
+        if (path != null ? !path.equals(videoInfo.path) : videoInfo.path != null) return false;
+        return true;
+
+    }
+
+    public boolean isThumbmail() {
+        if (TextUtils.isEmpty(getThumbmailLocalUrl()))
+            return false;
+        else if (new File(getThumbmailLocalUrl()).exists())
+            return true;
+        else return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = path != null ? path.hashCode() : 0;
+        result = 31 * result + (vedioName != null ? vedioName.hashCode() : 0);
+        result = 31 * result + (int) (duration ^ (duration >>> 32));
+        result = 31 * result + (int) (size ^ (size >>> 32));
+        result = 31 * result + (thumbnail != null ? thumbnail.hashCode() : 0);
+        result = 31 * result + (thumbmailLocalUrl != null ? thumbmailLocalUrl.hashCode() : 0);
+        result = 31 * result + (imgLocalUrl != null ? imgLocalUrl.hashCode() : 0);
+        result = 31 * result + (imgObjectKey != null ? imgObjectKey.hashCode() : 0);
+        result = 31 * result + (videoObjectKey != null ? videoObjectKey.hashCode() : 0);
+        result = 31 * result + (int) (Date ^ (Date >>> 32));
+        return result;
+    }
+
+    public static List<VideoInfo> getVideoList() {
+        List<VideoInfo> infos = SQLite.select().from(VideoInfo.class).queryList();
+        return infos;
     }
 
     public static Observable<List<VideoInfo>> getAllVideos() {
