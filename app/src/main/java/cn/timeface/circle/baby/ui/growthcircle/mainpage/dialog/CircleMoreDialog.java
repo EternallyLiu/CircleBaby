e3baa71fedcc32676606d59f0937803713bd8b04
汪帅 <wangbin@timeface.cn>
2017-04-15 14:06:57 +0800

    修改邀请加圈的文案

--- app/src/main/java/cn/timeface/circle/baby/ui/growthcircle/mainpage/dialog/CircleMoreDialog.java
+++ app/src/main/java/cn/timeface/circle/baby/ui/growthcircle/mainpage/dialog/CircleMoreDialog.java
@@ -97,9 +97,9 @@ public class CircleMoreDialog extends DialogFragment {
                 CircleInfoActivity.open(getContext(), circleObj);
                 break;
             case R.id.tv_share:
-                new ShareDialog(getActivity()).share(FastData.getCircleUserInfo().getCircleNickName()+"请你加圈", GrowthCircleObj.getInstance().getCircleName()+FastData.getUserName()+"邀请你加入，在这里一起记录宝宝的学习成长时光！"
-                        , ShareSdkUtil.getImgStrByResource(getActivity(), R.drawable.ic_laucher_quadrate),
-                        BuildConfig.API_URL+"growthCircleShare/index.html?circleId="+circleObj.getCircleId()+"&circleMemberId="+ FastData.getCircleUserInfo().getCircleUserId());
+                new ShareDialog(getActivity()).share(FastData.getCircleUserInfo().getCircleNickName() + "请你加圈", String.format("%s邀请你加入%s，一起记录宝宝的学习成长时光！ ", FastData.getCircleUserInfo().getCircleNickName(), FastData.getCircleObj().getCircleName())
+                        , FastData.getCircleObj().getCircleCoverUrl(),
+                        BuildConfig.API_URL + "growthCircleShare/index.html?circleId=" + circleObj.getCircleId() + "&circleMemberId=" + FastData.getCircleUserInfo().getCircleUserId());
                 break;
             case R.id.tv_production:
                 CircleBookActivity.open(getActivity(), circleObj.getCircleId());
